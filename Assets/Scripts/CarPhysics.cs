74c68e6316cfd66423a0979dd4dbdc93b157f99a
FireLordBulb <79010333+FireLordBulb@users.noreply.github.com>
2023-09-28 10:44:08 +0200

    OneDrive fix 2, electric boogaloo

--- Assets/Scripts/CarPhysics.cs
+++ Assets/Scripts/CarPhysics.cs
@@ -6,12 +6,14 @@ using UnityEngine.InputSystem;
 
 public class CarPhysics : MonoBehaviour {
 	[SerializeField] private PhysicsConfig physicsConfig;
-	[SerializeField] private InputAction gas, steering;
-	private Vector2 gasForce;
+	// TODO InputActionAsset
+	[SerializeField] private InputAction gas, reverse, steering;
+	private Vector2 gasForce, reverseForce;
 	private float steerTorque;
 	private Rigidbody2D rigidBody;
 	private void Start(){
 		gasForce = new Vector2(physicsConfig.GasForce, 0);
+		reverseForce = new Vector2(physicsConfig.ReverseForce, 0);
 		steerTorque = physicsConfig.SteerTorque;
 		transform.localScale = physicsConfig.CarSize;
 		rigidBody = gameObject.GetComponent<Rigidbody2D>();
@@ -20,16 +22,21 @@ public class CarPhysics : MonoBehaviour {
 	}
 	private void OnEnable(){
 		gas.Enable();
+		reverse.Enable();
 		steering.Enable();
 	}
 	private void OnDisable(){
-		gas.Enable();
-		steering.Enable();
+		gas.Disable();
+		reverse.Disable();
+		steering.Disable();
 	}
 	private void FixedUpdate(){
 		if (gas.IsPressed()){
 			rigidBody.AddRelativeForce(gasForce);
+		} else if (reverse.IsPressed()){
+			rigidBody.AddRelativeForce(reverseForce);
 		}
+		
 		if (steering.IsPressed()){
 			rigidBody.AddTorque(steerTorque*steering.ReadValue<float>());
 		}
--- Assets/Scripts/PhysicsConfig.cs
+++ Assets/Scripts/PhysicsConfig.cs
@@ -6,10 +6,11 @@ using UnityEngine.Serialization;
 [CreateAssetMenu]
 public class PhysicsConfig : ScriptableObject {
 	[SerializeField] private Vector2 carSize;
-	[SerializeField] private float linearDrag, angularDrag, gasForce, steerTorque;
+	[SerializeField] private float linearDrag, angularDrag, gasForce, reverseForce, steerTorque;
 	public Vector2 CarSize => carSize;
 	public float LinearDrag => linearDrag;
 	public float AngularDrag => angularDrag;
 	public float GasForce => gasForce;
+	public float ReverseForce => reverseForce;
 	public float SteerTorque => steerTorque;
 }
